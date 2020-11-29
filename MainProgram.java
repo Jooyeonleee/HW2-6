61007f06632df69652eaf5d5964bcee545d34ffc
Bon Kim <qhsl0615@naver.com>
2020-11-30 00:54:32 +0900

    add bon's code!

--- MainProgram.java
+++ MainProgram.java
@@ -23,9 +23,58 @@ public class MainProgram {
     }
 
     public void runModule(int studentID) {
-        if (studentID == 11530)
+		if (studentID == 11530)
             System.out.println("No. It is the professor ID.\n");
-        else
-            System.out.println("To be developed...\n");
+        else {
+        	if(studentID == 1810954) {
+            	while (true) {
+            		System.out.println("1. Calculate the maximum of three");
+                    System.out.println("2. Calculate the minimum of three");
+                    System.out.println("3. Exit");
+                    Scanner sc = new Scanner(System.in);
+                    
+                    System.out.print("Enter menu number : ");
+                    int select = sc.nextInt();
+                    
+                    if(select == 1 || select == 2) {
+                    	System.out.print("Enter a num1 : ");
+       	            	int num1 = sc.nextInt();
+       	            	System.out.print("Enter a num2 : ");
+       	            	int num2 = sc.nextInt();
+       	            	System.out.print("Enter a num3 : ");
+       	            	int num3 = sc.nextInt();
+       	            	if(select == 1)
+       	            		System.out.println("Answer : " + maxOfThree(num1, num2, num3)); 
+       	            	else if(select == 2)
+                        	System.out.println("Answer : " + minOfThree(num1, num2, num3));
+                    }else if(select == 3) break;
+                    else System.out.println("Please enter only 1 or 2 or 3");
+                }
+        	}
+        	
+        	
+        	System.out.println("To be developed...\n");
+        }
+    }
+    
+    public int maxOfThree(int a, int b, int c) {
+    	int maxNum = a;
+    	if (maxNum < b || maxNum < c) {
+    		maxNum = b;
+    		if (maxNum < c) {
+    			maxNum = c;
+    		}
+    	}
+    	return maxNum;
+    }
+    public int minOfThree(int a, int b, int c) {
+    	int minNum = a;
+    	if (minNum > b || minNum > c) {
+    		minNum = b;
+    		if (minNum > c) {
+    			minNum = c;
+    		}
+    	}
+    	return minNum;
     }
 }
