8832cc1d1f97674416ff62acb4d5b6536df037e5
Bon Kim <qhsl0615@naver.com>
2020-11-30 02:10:36 +0900

    modify a structure

--- MainProgram.java
+++ MainProgram.java
@@ -23,36 +23,33 @@ public class MainProgram {
     }
 
     public void runModule(int studentID) {
-		if (studentID == 11530)
+	if (studentID == 11530)
             System.out.println("No. It is the professor ID.\n");
         else {
         	if(studentID == 1810954) {
-            	while (true) {
-            		System.out.println("[Student ID: "+studentID+"]");
-            		System.out.println("1. Calculate the maximum of three");
-                    System.out.println("2. Calculate the minimum of three");
-                    System.out.println("3. Exit");
-                    Scanner sc = new Scanner(System.in);
-                    
-                    System.out.print("Enter menu number : ");
-                    int select = sc.nextInt();
-                    
-                    if(select == 1 || select == 2) {
-                    	System.out.print("Enter a num1 : ");
-       	            	int num1 = sc.nextInt();
-       	            	System.out.print("Enter a num2 : ");
-       	            	int num2 = sc.nextInt();
-       	            	System.out.print("Enter a num3 : ");
-       	            	int num3 = sc.nextInt();
-       	            	if(select == 1)
-       	            		System.out.println("Answer : " + maxOfThree(num1, num2, num3)); 
-       	            	else if(select == 2)
-                        	System.out.println("Answer : " + minOfThree(num1, num2, num3));
-                    }else if(select == 3) break;
-                    else System.out.println("Please enter only 1 or 2 or 3");
-                }
+			System.out.println("[Student ID: "+studentID+"]");
+        		System.out.println("1. Calculate the maximum of three");
+                System.out.println("2. Calculate the minimum of three");
+               
+                Scanner sc = new Scanner(System.in);
+                
+                System.out.print("Enter menu number : ");
+                int select = sc.nextInt();
+                
+                System.out.print("Enter a num1 : ");
+   	            int num1 = sc.nextInt();
+   	            System.out.print("Enter a num2 : ");
+   	            int num2 = sc.nextInt();
+   	            System.out.print("Enter a num3 : ");
+   	            int num3 = sc.nextInt();
+   	            
+   	            if(select == 1)
+   	            	System.out.println("Result : " + maxOfThree(num1, num2, num3)); 
+   	            else if(select == 2)
+                    System.out.println("Result : " + minOfThree(num1, num2, num3));
+                else 
+                	System.out.println("Please enter only 1 or 2");
         	}
-        	
         	
         	System.out.println("To be developed...\n");
         }
